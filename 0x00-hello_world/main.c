2ebb7003a7e22b95e87d54ad700d3f00ff824e08
kmayita <kubitimaya.scope@gmail.com>
2022-09-09 13:14:51 +0000

    compiler

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,20 @@
+i#include <stdio.h>
+
 #include <stdio.h>
+
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-    return (0);
+fprintf(stderr, "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n");
+
+return (1);
 }
+
+
