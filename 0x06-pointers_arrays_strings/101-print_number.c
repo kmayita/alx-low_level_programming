f3ac79ebbd285d723a75f380a9cef40ca39d8073
kmayita <umamaya7766@gmail.com>
2022-09-23 06:03:56 +0000

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,11 @@
-#include "main.h"
 
-/**
- * print_number - prints an int
- * @n: input int
- * Return: nothing
- */
-void print_number(int n)
-{
-unsigned int m, d, count;
-
-if (n < 0)
-{
-_putchar(45);
-m = n * -1;
-}
-else
-{
-m = n;
-}
-
-d = m;
-count = 1;
-
-while (d > 9)
-{
-d /= 10;
-count *= 10;
-}
-
-for (; count >= 1; count /= 10)
-{
-_putchar(((m / count) % 10) + 48);
-}
-}
+    #include<stdio.h>  
+    #include<conio.h>  
+    int main()  
+    {  
+    int a;  
+    printf("Enter an integer\n");  
+    scanf("%d",&a);  
+    printf("\n Enter an integer that you have entered %d",a);  
+    return 0;  
+    }  
